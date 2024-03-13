25268eaff82b6680ac48a390a7f6c4d37b732057
Miguel Cortes <miguel.cortes@ias.com.co>
2024-03-12 20:55:27 -0500

    init project

---
+++ src/test/java/co/com/tkl/shopapp/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.tkl.shopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
